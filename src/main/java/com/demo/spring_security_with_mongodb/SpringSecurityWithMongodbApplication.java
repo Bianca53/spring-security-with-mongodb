180275d4930a82f3f9b500401353dfcfc9ac71ac
Bianca <bbia754@gmail.com>
2026-01-28 23:49:24 -0300

    Initial commit

---
+++ src/main/java/com/demo/spring_security_with_mongodb/SpringSecurityWithMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.demo.spring_security_with_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWithMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityWithMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/spring_security_with_mongodb/SpringSecurityWithMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.spring_security_with_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityWithMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
